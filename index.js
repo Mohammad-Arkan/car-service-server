39818e6c7278d93ccc6670939085774bc9960e57
Mohammad Arkan <a.r.khan.coxyboys@gmail.com>
2023-08-22 17:55:51 +0600

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000
@@ -27,13 +27,25 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('CarCarCar').collection('service');
+
     app.get('/service', async(req, res)=> {
       const cursor = serviceCollection.find()
       const result = await cursor.toArray();
       res.send(result);
     })
 
+    app.get('/service/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
 
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {  title: 1, service_id: 1, price:1,  },
+      };
+
+      const result = await serviceCollection.findOne(query, options)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
